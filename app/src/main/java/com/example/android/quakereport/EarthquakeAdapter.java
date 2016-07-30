0c43dbf1ca31c2dc8db4ea1bdeeb91dc74ab09a4
Luis Perez <lperez.1092@gmail.com>
2016-07-30 19:24:19 -0430

    Divide el texto de location en dos TextViews

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -14,6 +14,9 @@ import java.util.Date;
 import java.util.Locale;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public static final String LOCATION_SEPARATOR = " of ";
+
     /**
      * Constructs a new {@link EarthquakeAdapter}
      *
@@ -29,7 +32,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItemView = convertView;
         if (listItemView == null) {
-            listItemView =  LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item,
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item,
                     parent, false);
         }
         Earthquake currentEarthquake = getItem(position);
@@ -37,8 +40,13 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
         magnitudeTextView.setText(currentEarthquake.getMagnitude());
 
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
-        locationTextView.setText(currentEarthquake.getLocation());
+        String[] locationParts = splitLocation(currentEarthquake.getLocation());
+
+        TextView locationOffsetTextView = (TextView) listItemView.findViewById(R.id.location_offset);
+        locationOffsetTextView.setText(locationParts[0]);
+
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.primary_location);
+        locationTextView.setText(locationParts[1]);
 
         // Create a new Date object from the time in milliseconds of the earthquake
         Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
@@ -71,5 +79,18 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String formatTime(Date dateObject) {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a", Locale.US);
         return timeFormat.format(dateObject);
+    }
+
+    private String[] splitLocation(String location) {
+        String[] result = new String[2];
+        if (location.contains(LOCATION_SEPARATOR)) {
+            int index = location.indexOf(LOCATION_SEPARATOR);
+            result[0] = location.substring(0, index + 3);
+            result[1] = location.substring(index + 4);
+        } else {
+            result[0] = getContext().getString(R.string.near_the);
+            result[1] = location;
+        }
+        return result;
     }
 }
